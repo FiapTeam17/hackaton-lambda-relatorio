2dfd7c16939758493ec171044b2d1a9b6f825898
Jefferson Nunes <jeffersonvnunes@gmail.com>
2024-03-22 22:27:57 -0300

    feat: pesquisa banco

---
+++ HackathonFiap.Lambda.Relatorio/src/HackathonFiap.Lambda.Relatorio/Context/DatabaseContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HackathonFiap.Lambda.Relatorio.Context;
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+}
--- HackathonFiap.Lambda.Relatorio/src/HackathonFiap.Lambda.Relatorio/Function.cs
+++ HackathonFiap.Lambda.Relatorio/src/HackathonFiap.Lambda.Relatorio/Function.cs
@@ -1,6 +1,9 @@
+using System.Data.Common;
 using System.Text.Json;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
+using HackathonFiap.Lambda.Relatorio.Context;
+using Microsoft.EntityFrameworkCore;
 
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -10,6 +13,10 @@ namespace HackathonFiap.Lambda.Relatorio;
 
 public class Function
 {
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+    private readonly DbConnection _connection;
+    private DbCommand? _dbCommand;
+    
     /// <summary>
     /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
     /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
@@ -17,6 +24,13 @@ public class Function
     /// </summary>
     public Function()
     {
+        _jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+
+        };
+
+        _connection = MontarConnectionOperacao();
     }
 
 
@@ -28,26 +42,67 @@ public class Function
     /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
     /// <returns></returns>
     public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
-    {
+    {   
+        await _connection.OpenAsync();
+        
         foreach (var message in evnt.Records)
         {
             await ProcessMessageAsync(message, context);
         }
+
+        await _connection.CloseAsync();
     }
 
     private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
-    {
-        var jsonSerializerOptions = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true,
-
-        };
-        
+    {   
         context.Logger.LogInformation($"Processed message {message.Body}");
-        var periodoDto = JsonSerializer.Deserialize<PeriodoDto>(message.Body, jsonSerializerOptions);
-        context.Logger.LogInformation($"Ano {periodoDto.Ano}");
-        context.Logger.LogInformation($"Mes {periodoDto.Mes}");
-        // TODO: Do interesting work based on the new message
-        await Task.CompletedTask;
+        var periodoDto = JsonSerializer.Deserialize<PeriodoDto>(message.Body, _jsonSerializerOptions);
+        
+        await using var command = _connection.CreateCommand();
+
+        command.CommandText = "select p.Id, p.Horario from ponto p where p.FuncionarioId = @FuncionarioId and year(p.Horario) = @ano and month(p.Horario) = @mes";
+        
+        var param = command.CreateParameter();
+        param.ParameterName = "@FuncionarioId";
+        param.Value = periodoDto.FuncionarioId;
+        command.Parameters.Add(param);
+                
+        param = command.CreateParameter();
+        param.ParameterName = "@ano";
+        param.Value = periodoDto.Ano;
+        command.Parameters.Add(param);
+        
+        param = command.CreateParameter();
+        param.ParameterName = "@mes";
+        param.Value = periodoDto.Mes;
+        command.Parameters.Add(param);
+        
+        await command.ExecuteNonQueryAsync();
+
+        var listaPonto = new List<PontoModel>();
+        
+        var read = await command.ExecuteReaderAsync();
+        while (await read.ReadAsync())
+        {
+            
+            listaPonto.Add(new PontoModel
+            {
+                Id = read.GetGuid(0),
+                Horario = read.GetDateTime(1),
+                FuncionarioId = periodoDto.FuncionarioId
+            });
+        }
+        await read.CloseAsync();
+    }
+    
+    private DbConnection MontarConnectionOperacao()
+    {
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            
+        var options = new DbContextOptionsBuilder<DatabaseContext>()
+            .UseMySQL(connectionString, op => op.CommandTimeout(600)).Options;
+        var db = new DatabaseContext(options);
+
+        return db.Database.GetDbConnection();
     }
 }
